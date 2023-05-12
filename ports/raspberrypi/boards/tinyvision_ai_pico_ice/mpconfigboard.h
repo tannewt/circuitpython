e9b26f1b8c639f842c75f38240c32b6e645d80ab
Scott Shawcroft <scott@tannewt.org>
2023-05-12 14:56:42 -0700

    Add pico ice board def

---
+++ ports/raspberrypi/boards/tinyvision_ai_pico_ice/mpconfigboard.h
@@ -0,0 +1,8 @@
+#define MICROPY_HW_BOARD_NAME "TinyVision AI Pico Ice"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO10)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO11)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO8)
+
+#define MICROPY_HW_LED_STATUS (&pin_GPIO12)
