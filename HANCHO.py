49eef7eaf1bb69da5ef64b71a19175e9817c58cf
Scott Shawcroft <scott@tannewt.org>
2024-03-13 22:31:55 -0700

    hancho update and multicore ninja

--- HANCHO.py
+++ HANCHO.py
@@ -3,10 +3,10 @@ import pathlib
 
 CPU_FLAGS = {
     "cortex-m0+": "--target=arm-none-eabi -march=armv6-m -mthumb -mabi=aapcs-linux -mcpu=cortex-m0plus -msoft-float -mfloat-abi=soft"
-
 }
 
 hancho.config.task_dir = "{ root_dir / rule_dir }"
+hancho.config.deps_dir = "{ root_dir / rule_dir }"
 hancho.config.in_dir = "{ root_dir / call_dir }"
 hancho.config.out_dir = "{ root_dir / build_dir / call_dir }"
 
@@ -32,42 +32,51 @@ preprocess = hancho.Rule(
 )
 
 split_defs = hancho.Rule(
-  desc = "Splitting {mode} defs",
-  command = "python py/makeqstrdefs.py split {mode} {files_in} {build_dir}/genhdr/{mode} {files_out}",
-  deps="py/makeqstrdefs.py",
-  files_out="genhdr/{mode}/{swap_ext(files_in, '.split')}.{mode}",
+    desc="Splitting {mode} defs",
+    command="python py/makeqstrdefs.py split {mode} {files_in} {build_dir}/genhdr/{mode} {files_out}",
+    deps="py/makeqstrdefs.py",
+    files_out="genhdr/{mode}/{swap_ext(files_in, '.split')}.{mode}",
 )
 
 collect_defs = hancho.Rule(
-  desc = "Collecting {mode} defs",
-  command = ["rm -f {files_out}.hash", "python py/makeqstrdefs.py cat {mode} _ {build_dir}/genhdr/{mode} {files_out}"],
-  deps="py/makeqstrdefs.py",
+    desc="Collecting {mode} defs",
+    command=[
+        "rm -f {files_out}.hash",
+        "python py/makeqstrdefs.py cat {mode} _ {build_dir}/genhdr/{mode} {files_out}",
+    ],
+    deps="py/makeqstrdefs.py",
 )
 
 python_script = hancho.Rule(
-    command = "python {deps} {files_in} > {files_out}",
+    command="python {deps} {files_in} > {files_out}",
 )
 
 qstr_preprocessed = hancho.Rule(
     desc="Preprocess QSTRs",
-    command='cat {files_in} | sed \'s/^Q(.*)/"&"/\' | {compiler} -DNO_QSTR {cflags} -E - | sed \'s/^\"\\(Q(.*)\\)\"/\1/\' > {files_out}',
-    deps=[""]
+    command="cat {files_in} | sed 's/^Q(.*)/\"&\"/' | {compiler} -DNO_QSTR {cflags} -E - | sed 's/^\"\\(Q(.*)\\)\"/\1/' > {files_out}",
+    deps=[""],
 )
 
-qstr_generated = python_script.extend(
-    deps="py/makeqstrdata.py"
-)
+qstr_generated = python_script.extend(deps="py/makeqstrdata.py")
 
 version_generate = hancho.Rule(
     desc="Generate version info",
     command="python py/makeversionhdr.py {files_out}",
-    deps="py/makeversionhdr.py"
-  )
+    deps="py/makeversionhdr.py",
+)
 
 version_header = version_generate([], files_out="genhdr/mpversion.h")
 
+
 def board(
-    board_name, cpu, compiler="clang", port_flags="", flash_filesystem="external", tusb_mem_align=4, usb=False, **kwargs
+    board_name,
+    cpu,
+    compiler="clang",
+    port_flags="",
+    flash_filesystem="external",
+    tusb_mem_align=4,
+    usb=False,
+    **kwargs,
 ):
     print("top level", board_name)
 
@@ -78,7 +87,10 @@ def board(
 
     cpu_flags = CPU_FLAGS[cpu]
 
-    picolibc_includes, picolibc_a = picolibc.build(hancho.config.root_dir / hancho.config.build_dir / "lib" / "picolibc" / cpu, cpu_flags)
+    picolibc_includes, picolibc_a = picolibc.build(
+        hancho.config.root_dir / hancho.config.build_dir / "lib" / "picolibc" / cpu,
+        cpu_flags,
+    )
 
     circuitpython_flags = []
     circuitpython_flags.append("-Wno-expansion-to-defined")
@@ -86,61 +98,103 @@ def board(
     for module in always_on_modules:
         circuitpython_flags.append(f"-DCIRCUITPY_{module.upper()}=1")
     circuitpython_flags.append(f"-DCIRCUITPY_USB={1 if usb else 0}")
-    circuitpython_flags.append(f"-DCIRCUITPY_BOARD_ID=\\\"{board_id}\\\"")
+    circuitpython_flags.append(f'-DCIRCUITPY_BOARD_ID=\\"{board_id}\\"')
     circuitpython_flags.append(f"-DCIRCUITPY_TUSB_MEM_ALIGN={tusb_mem_align}")
-    circuitpython_flags.append("-DFFCONF_H=\\\"lib/oofatfs/ffconf.h\\\"")
+    circuitpython_flags.append('-DFFCONF_H=\\"lib/oofatfs/ffconf.h\\"')
     circuitpython_flags.append("-I {root_dir}")
     circuitpython_flags.append(f"-I lib/tinyusb/src")
     circuitpython_flags.append(f"-isystem lib/cmsis/inc")
     circuitpython_flags.append("-isystem lib/picolibc/newlib/libc/include/")
-    circuitpython_flags.append(f"-I {hancho.config.build_dir}")
+    circuitpython_flags.append(f"-I {hancho.config.build_dir}/{board_id}")
     for picolibc_include_path in picolibc_includes:
         circuitpython_flags.extend(("-isystem ", picolibc_include_path))
 
     for fs_type in ["internal", "external", "qspi"]:
-      value = 1 if flash_filesystem == fs_type else 0
-      circuitpython_flags.append(f"-D{fs_type.upper()}_FLASH_FILESYSTEM={value}")
+        value = 1 if flash_filesystem == fs_type else 0
+        circuitpython_flags.append(f"-D{fs_type.upper()}_FLASH_FILESYSTEM={value}")
 
-    supervisor_source = ["main.c", "lib/tlsf/tlsf.c", f"ports/{port_id}/supervisor/port.c", "supervisor/stub/misc.c"] + hancho.glob("supervisor/shared/**/*.c")
+    supervisor_source = [
+        "main.c",
+        "lib/tlsf/tlsf.c",
+        f"ports/{port_id}/supervisor/port.c",
+        "supervisor/stub/misc.c",
+    ] + hancho.glob("supervisor/shared/**/*.c")
 
     source_files = supervisor_source + ["py/modsys.c", "extmod/vfs.c"]
-    board_output = f"{{ root_dir / build_dir / \"{board_id}\" }}"
-    preprocessed = [preprocess(f,
+    board_output = f'{{ root_dir / build_dir / "{board_id}" }}'
+    preprocessed = [
+        preprocess(
+            f,
             compiler=compiler,
             out_dir=board_output,
             qstr_flags="-DNO_QSTR",
             deps=[version_header, picolibc_a],
             circuitpython_flags=circuitpython_flags,
-            port_flags=port_flags) for f in source_files]
+            port_flags=port_flags,
+        )
+        for f in source_files
+    ]
     board_split_defs = split_defs.extend(out_dir=board_output)
     qstr_split = [board_split_defs(f, mode="qstr") for f in preprocessed]
     modules_split = [board_split_defs(f, mode="module") for f in preprocessed]
-    root_pointers_split = [board_split_defs(f, mode="root_pointer") for f in preprocessed]
+    root_pointers_split = [
+        board_split_defs(f, mode="root_pointer") for f in preprocessed
+    ]
 
     board_collect_defs = collect_defs.extend(out_dir=board_output)
-    qstr_collected = board_collect_defs(files_in=qstr_split, files_out="genhdr/qstrdefs.collected", mode="qstr")
-    modules_collected = board_collect_defs(files_in=modules_split, files_out="genhdr/moduledefs.collected", mode="module")
-    root_pointers_collected = board_collect_defs(files_in=root_pointers_split, files_out="genhdr/root_pointers.collected", mode="root_pointer")
+    qstr_collected = board_collect_defs(
+        files_in=qstr_split, files_out="genhdr/qstrdefs.collected", mode="qstr"
+    )
+    modules_collected = board_collect_defs(
+        files_in=modules_split, files_out="genhdr/moduledefs.collected", mode="module"
+    )
+    root_pointers_collected = board_collect_defs(
+        files_in=root_pointers_split,
+        files_out="genhdr/root_pointers.collected",
+        mode="root_pointer",
+    )
 
     board_python_script = python_script.extend(out_dir=board_output)
-    module_header = board_python_script(modules_collected, "genhdr/moduledefs.h", deps="py/makemoduledefs.py")
+    module_header = board_python_script(
+        modules_collected, "genhdr/moduledefs.h", deps="py/makemoduledefs.py"
+    )
 
-    qstrpp = qstr_preprocessed(files_in=["py/qstrdefs.h"], files_out="genhdr/qstrdefs.preprocessed.h", compiler=compiler, cflags=[circuitpython_flags, port_flags], out_dir=board_output)
-    qstrdefs = qstr_generated(files_in=[qstrpp, qstr_collected], files_out="genhdr/qstrdefs.generated.h", out_dir=board_output)
+    qstrpp = qstr_preprocessed(
+        files_in=["py/qstrdefs.h"],
+        files_out="genhdr/qstrdefs.preprocessed.h",
+        compiler=compiler,
+        cflags=[circuitpython_flags, port_flags],
+        out_dir=board_output,
+    )
+    qstrdefs = qstr_generated(
+        files_in=[qstrpp, qstr_collected],
+        files_out="genhdr/qstrdefs.generated.h",
+        out_dir=board_output,
+    )
 
-    root_pointers = board_python_script(files_in=root_pointers_collected, files_out="genhdr/root_pointers.h", deps="py/make_root_pointers.py")
+    root_pointers = board_python_script(
+        files_in=root_pointers_collected,
+        files_out="genhdr/root_pointers.h",
+        deps="py/make_root_pointers.py",
+    )
 
     objects = [picolibc_a]
     for source_file in source_files:
         objects.append(
             compile_circuitpython(
                 source_file,
-                deps=[qstrdefs, version_header, root_pointers, module_header, picolibc_a],
+                deps=[
+                    qstrdefs,
+                    version_header,
+                    root_pointers,
+                    module_header,
+                    picolibc_a,
+                ],
                 compiler=compiler,
                 cpu_flags=cpu_flags,
                 circuitpython_flags=circuitpython_flags,
                 port_flags=port_flags,
-                out_dir=board_output
+                out_dir=board_output,
             )
         )
 
--- lib/picolibc.py
+++ lib/picolibc.py
@@ -1,23 +1,31 @@
 from hancho import *
 
 meson = Rule(
-    command = "meson setup --reconfigure -Dtests=false -Dmultilib=false --cross-file {cross_file} picolibc {out_dir}",
-    files_out = ["build.ninja"],
+    command="meson setup --reconfigure -Dtests=false -Dmultilib=false --cross-file {cross_file} picolibc {out_dir}",
+    files_out=["build.ninja"],
 )
 
 ninja = Rule(
-    command = "ninja -j 1",
-    out_dir = "{task_dir}"
+    command="ninja -j 8",
+    out_dir="{task_dir}",
+    job_count=8,
 )
+
 
 def build(build_dir, cflags="", compiler="clang"):
     include_paths = [build_dir, "lib/picolibc/newlib/libc/stdio"]
     libraries = []
 
+    ninja_file = meson(
+        "picolibc/meson.build",
+        out_dir=build_dir,
+        cross_file="picolibc/scripts/cross-clang-thumbv6m-none-eabi.txt",
+    )
 
-    ninja_file = meson("picolibc/meson.build", out_dir=build_dir, cross_file="picolibc/scripts/cross-clang-thumbv6m-none-eabi.txt")
-
-    picolibc_a = ninja(ninja_file, files_out=["newlib/libm.a", "newlib/libc.a", "newlib/libg.a"], task_dir=build_dir)
-
+    picolibc_a = ninja(
+        ninja_file,
+        files_out=["newlib/libm.a", "newlib/libc.a", "newlib/libg.a"],
+        task_dir=build_dir,
+    )
 
     return include_paths, picolibc_a
