097b684a5d46c598b4fbecbfab6187ef1bf36eaf
Scott Shawcroft <scott@tannewt.org>
2024-04-10 16:30:39 -0700

    latest & greatest

--- lib/build_picolibc.py
+++ lib/build_picolibc.py
@@ -1,6 +1,8 @@
 import argparse
 import inspect
 
+from typing import Optional
+
 try:
     import cmsis_pack_manager as cmsis_packs
 except ImportError:
@@ -9,11 +11,61 @@ except ImportError:
 class Microcontroller:
     pass
 
+class Compiler:
+    pass
+
 class CPU:
     pass
 
-class Compiler:
-    pass
+    def get_arch_cflags(self, compiler: Compiler) -> str:
+        return ""
+
+class ARM(CPU):
+    def __init__(self, mcpu: str, floating_point: bool = False):
+        self.mcpu = mcpu
+        self.floating_point = floating_point
+
+    def get_arch_cflags(self, compiler: Compiler) -> str:
+        flags = f"-mcpu={self.mcpu} -mthumb"
+        if self.floating_point:
+            flags += " -mfloat-abi=hard -mfpu=auto"
+        return flags
+
+    @staticmethod
+    def from_pdsc(description: dict) -> Optional[CPU]:
+        if "core" in description:
+            core = description["core"]
+            print(core)
+            if core == "CortexM0Plus":
+                print("cm0")
+                return CortexM0Plus()
+            elif core == "CortexM4":
+                return CortexM4(description["fpu"] != "None")
+        return None
+
+class CortexM0Plus(ARM):
+    def __init__(self, small_multiply=False):
+        if small_multiply:
+            small_multiply = ".small-multiply"
+        else:
+            small_multiply = ""
+        super().__init__("cortex-m0plus" + small_multiply, False)
+
+class CortexM4(ARM):
+    def __init__(self, floating_point: bool):
+        if floating_point:
+            fp = ""
+        else:
+            fp = "+nofp"
+        super().__init__("cortex-m4" + fp, floating_point)
+
+class CortexM7(ARM):
+    def __init__(self, floating_point: str):
+        if floating_point:
+            fp = ""
+        else:
+            fp = "+nofp"
+        super().__init__("cortex-m7" + fp, floating_point)
 
 def embedded_build(function):
     parser = argparse.ArgumentParser()
@@ -43,11 +95,15 @@ def embedded_build(function):
                     print(device_info["processor"])
                 if "processors" in device_info:
                     for processor in device_info["processors"]:
+                        if "svd" in processor:
+                            del processor["svd"]
                         if target_processor is None:
                             target_processor = processor
                         elif target_processor != processor:
                             print("mismatched processor", target_processor, processor)
         print(target_processor)
+        cpu = ARM.from_pdsc(target_processor)
+        print(cpu.get_arch_cflags(None))
 
 
 
