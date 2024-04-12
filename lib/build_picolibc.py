674d3b8078ec554e1569c19b8196f718ad7afd2c
Scott Shawcroft <scott@tannewt.org>
2024-04-12 13:30:30 -0700

    riscv builds

--- lib/build_picolibc.py
+++ lib/build_picolibc.py
@@ -3,6 +3,7 @@ import inspect
 import pathlib
 import shlex
 import subprocess
+import sys
 
 from typing import Optional
 
@@ -24,10 +25,21 @@ class CPU:
         return ""
 
 class RISCV(CPU):
-    def __init__(self, extensions: set[str], bits=32):
+    def __init__(self, extensions: tuple[str], bits=32):
         self.extensions = extensions
         self.unique_id = "rv" + str(bits) + "".join(extensions)
         self.bits = bits
+
+    def get_arch_cflags(self, compiler: Compiler) -> list[str]:
+        joined = "_".join(self.extensions).lower()
+        flags = [f"--target=riscv{self.bits}", f"-march=rv{self.bits}{joined}"]
+        return flags
+
+class Hazard3(RISCV):
+    def __init__(self):
+        super().__init__(("I", "M", "A", "C", "Zicsr", "Zba", "Zbb", "Zbc", "Zbs", "Zbkb", "Zcb", "Zcmp"))
+        self.unique_id = "hazard3"
+
 
 class ARM(CPU):
     def __init__(self, mcpu: str, floating_point: bool = False):
@@ -94,57 +106,82 @@ class CortexM33(ARM):
             dsp_flag = "+nodsp"
         super().__init__("cortex-m33" + fp + dsp_flag, floating_point)
 
+def get_cpu_from_mcu(substr):
+    if not cmsis_packs:
+        return None
+
+    print("looking in cmsis packs")
+    cmsis_cache = cmsis_packs.Cache(True, False)
+
+    target_processor = None
+    for part in cmsis_cache.index.keys():
+        if substr in part:
+            # print(part)
+            device_info = cmsis_cache.index[part]
+            if "processor" in device_info:
+                print(device_info["processor"])
+            if "processors" in device_info:
+                for processor in device_info["processors"]:
+                    if "svd" in processor:
+                        del processor["svd"]
+                    if target_processor is None:
+                        target_processor = processor
+                    elif target_processor != processor:
+                        print("mismatched processor", target_processor, processor)
+    print(target_processor)
+    cpu = ARM.from_pdsc(target_processor)
+    print(cpu.get_arch_cflags(None))
+    return cpu
+
+def get_cpu_from_name(name):
+    name = name.lower()
+    if name == "hazard3":
+        return Hazard3()
+    elif name == "riscv32imac":
+        return RISCV(("I", "M", "A", "C"))
+    elif name in ("cm0+", "cortex-m0plus", "cortex-m0+"):
+        return CortexM0Plus()
+    elif name in ("cm4", "cortex-m4"):
+        return CortexM4(False)
+    elif name in ("cm4f", "cortex-m4f"):
+        return CortexM4(True)
+    elif name in ("cm7", "cortex-m7"):
+        return CortexM7(False)
+
 def embedded_build(function):
     function_args = []
     parser = argparse.ArgumentParser()
     for param in inspect.signature(function).parameters.values():
         if param.annotation == CPU:
-            target_help = "cpu target name"
-            if cmsis_packs:
-                target_help += " or ARM microcontroller name"
-            parser.add_argument("-t", "--target", type=str,
-                    help=target_help)
-            function_args.append("target")
+            cpu_help = "cpu target name"
+            parser.add_argument("-c", "--cpu", type=str,
+                    help=cpu_help)
+            function_args.append("cpu")
+        if param.annotation == CPU or param.annotation == Microcontroller:
+            mcu_help = "mcu target name"
+            parser.add_argument("-m", "--mcu", type=str,
+                    help=mcu_help)
+            if param.annotation == Microcontroller:
+                function_args.append("mcu")
 
         # print(param, type(param), param.annotation)
 
     cli_args = parser.parse_args()
 
     for i, farg in enumerate(function_args):
-        if getattr(cli_args, farg) is None:
-            parser.print_help()
-            return
-
-        if farg == "target":
-            print(cli_args.target)
-            if cmsis_packs:
-                print("looking in cmsis packs")
-                cmsis_cache = cmsis_packs.Cache(True, False)
-
-                target_processor = None
-                for part in cmsis_cache.index.keys():
-                    if cli_args.target in part:
-                        # print(part)
-                        device_info = cmsis_cache.index[part]
-                        if "processor" in device_info:
-                            print(device_info["processor"])
-                        if "processors" in device_info:
-                            for processor in device_info["processors"]:
-                                if "svd" in processor:
-                                    del processor["svd"]
-                                if target_processor is None:
-                                    target_processor = processor
-                                elif target_processor != processor:
-                                    print("mismatched processor", target_processor, processor)
-                print(target_processor)
-                cpu = ARM.from_pdsc(target_processor)
-                print(cpu.get_arch_cflags(None))
+        if farg == "cpu":
+            print(cli_args)
+            if cli_args.cpu:
+                function_args[i] = get_cpu_from_name(cli_args.cpu)
+            elif cli_args.mcu:
+                cpu = get_cpu_from_mcu(cli_args.mcu)
                 function_args[i] = cpu
 
     function(*function_args)
 
 def create_meson_cross_file(cpu: CPU):
     cflags = ",".join(f"'{flag}'" for flag in cpu.get_arch_cflags(None))
+    link_flags = cflags
     if isinstance(cpu, ARM):
         cpu_family = "arm"
         c_compiler = "arm-none-eabi-gcc"
@@ -157,6 +194,7 @@ def create_meson_cross_file(cpu: CPU):
         ar = "llvm-ar"
         strip = "llvm-strip"
         cpu_family = f"riscv{cpu.bits}"
+        link_flags += ",'-fuse-ld=lld', '-nostdlib'"
     else:
         raise RuntimeError("Unsupported CPU")
     return f"""[binaries]
@@ -172,8 +210,9 @@ cpu_family = '{cpu_family}'
 cpu = '{cpu_family}'
 endian = 'little'
 
-[properties]
+[built-in options]
 c_args = [{cflags}]
+c_link_args = [{link_flags}]
 """
 
 class Artifacts:
@@ -181,21 +220,28 @@ class Artifacts:
         return True
 
 def build(cpu: CPU) -> Artifacts:
-    source_dir = (pathlib.Path(__file__).parent / "picolibc").relative_to(pathlib.Path.cwd(), walk_up=True)
+    absolute_source_dir = pathlib.Path(__file__).parent / "picolibc"
+    if sys.version_info.minor >= 12:
+        source_dir = absolute_source_dir.relative_to(pathlib.Path.cwd(), walk_up=True)
+    else:
+        source_dir = absolute_source_dir.relative_to(pathlib.Path.cwd())
     build_path = pathlib.Path("build") / cpu.unique_id
     build_path.mkdir(parents=True, exist_ok=True)
     cross_file = build_path / "cross_file.txt"
     cross_file.write_text(create_meson_cross_file(cpu))
 
-    cmd = f"meson setup --reconfigure -Dtests=false -Dmultilib=false --cross-file {cross_file} {source_dir} {build_path}"
+    cmd = f"meson setup --reconfigure -Dsemihost=false -Dtests=false -Dmultilib=false --cross-file {cross_file} {source_dir} {build_path}"
     print(cmd)
-    subprocess.run(shlex.split(cmd), capture_output=True)
+    result = subprocess.run(shlex.split(cmd), capture_output=True)
+    if result.returncode != 0:
+        print(result.stdout.decode("utf-8"))
 
     cmd = f"ninja"
     print(cmd)
     result = subprocess.run(shlex.split(cmd), capture_output=True, cwd=build_path)
     print(result.returncode)
-    print(result.stdout)
+    if result.returncode != 0:
+        print(result.stdout.decode("utf-8"))
     return Artifacts()
 
 
