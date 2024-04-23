3a68c8973b846ae2903b7ecb367c7c1b7759d175
Scott Shawcroft <scott@tannewt.org>
2024-04-23 16:30:20 -0700

    consolidate more

--- ports/renode/tools/split_profile.py
+++ ports/renode/tools/split_profile.py
@@ -13,11 +13,38 @@ supervisor_f = supervisor.open("w")
 boot_py_f = boot_py.open("w")
 code_py_f = code_py.open("w")
 
-for line in input_file.open():
+
+def write(line):
     if "run_boot_py" in line:
         boot_py_f.write(line)
     if "run_code_py" in line:
         code_py_f.write(line)
+
+
+delay_stack = None
+delay_time = 0
+for line in input_file.open():
+    # consolidate delay calls except background tasks
+    if "mp_hal_delay_ms" in line and (
+        "background_callback_run" not in line
+        or line.rindex("mp_hal_delay_ms") > line.rindex("background_callback_run")
+    ):
+        split = line.rsplit(" ", 1)
+        if delay_stack is None:
+            delay_stack = split[0]
+        delay_time += int(split[1])
+        # print("drop", line.strip())
+        continue
+    elif delay_time > 0:
+        # print("bg", line.strip())
+        # if "mp_hal_delay_ms" in line:
+        #     print(line.rindex("mp_hal_delay_ms"), line.rindex("background_callback_run"))
+        # print(f"{delay_stack} {delay_time}")
+        write(f"{delay_stack} {delay_time}\n")
+        delay_time = 0
+        delay_stack = None
+
+    write(line)
 
 supervisor_f.close()
 boot_py_f.close()
