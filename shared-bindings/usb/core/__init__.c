15620cb40b4058207dfa363f48e13cf25fe487a4
Scott Shawcroft <scott@tannewt.org>
2025-04-02 12:38:01 -0700

    Correctly deinit USB devices

--- shared-bindings/usb/core/__init__.c
+++ shared-bindings/usb/core/__init__.c
@@ -89,7 +89,7 @@ static mp_obj_t _next_device(usb_core_devices_obj_t *iter) {
 
         // We passed the filters. Now make a properly allocated object to
         // return to the user.
-        usb_core_device_obj_t *self = mp_obj_malloc(usb_core_device_obj_t, &usb_core_device_type);
+        usb_core_device_obj_t *self = mp_obj_malloc_with_finaliser(usb_core_device_obj_t, &usb_core_device_type);
 
         common_hal_usb_core_device_construct(self, i);
         iter->next_index = i + 1;
