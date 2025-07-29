7ea1ecf681b134f5227f1283262f8f15a8399201
Mario Pesch <mario.pesch@uni-muenster.de>
2025-07-29 14:09:42 +0000

    include neopixel library

--- ports/espressif/boards/sensebox_eye_esp32s3/pins.c
+++ ports/espressif/boards/sensebox_eye_esp32s3/pins.c
@@ -46,6 +46,7 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_D48), MP_ROM_PTR(&pin_GPIO48) },
 
     { MP_ROM_QSTR(MP_QSTR_LED), MP_ROM_PTR(&pin_GPIO45) },
+    { MP_ROM_QSTR(MP_QSTR_NEOPIXEL), MP_ROM_PTR(&pin_GPIO45) },
 
     { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_GPIO43) },
     { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_GPIO44) },
