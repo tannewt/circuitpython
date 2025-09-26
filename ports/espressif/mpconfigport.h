f143e1d164c7e3b3ff5aa5dfcd8362d893e79862
Dan Halbert <halbert@halwitz.org>
2025-09-25 23:28:00 -0400

    ports/espressif/mpconfigport.h: remove commented code

--- ports/espressif/mpconfigport.h
+++ ports/espressif/mpconfigport.h
@@ -17,11 +17,6 @@
 
 #define CIRCUITPY_DIGITALIO_HAVE_INPUT_ONLY (1)
 
-// // Present SD card as USB MSC device by default
-// #ifndef CIRCUITPY_SDCARD_USB
-// #define CIRCUITPY_SDCARD_USB (1)
-// #endif
-
 #include "py/circuitpy_mpconfig.h"
 
 #define MICROPY_NLR_SETJMP                  (1)
