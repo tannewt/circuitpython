5028d2ce369afb9a083086be6086df3dbb657d87
Gustavo <gustavodiazm@gmail.com>
2025-10-28 18:33:56 -0400

    Remove fallback pin aliases; keep only user-accessible pins

--- ports/espressif/boards/waveshare_esp32_s3_touch_lcd_2_8/pins.c
+++ ports/espressif/boards/waveshare_esp32_s3_touch_lcd_2_8/pins.c
@@ -49,8 +49,8 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_BAT_ADC),     MP_ROM_PTR(&pin_GPIO8)  }, // VBAT sense (ADC)
 
     // UART header
-    { MP_ROM_QSTR(MP_QSTR_TX),        MP_ROM_PTR(&pin_GPIO43) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_RX),        MP_ROM_PTR(&pin_GPIO44) }, // User accessible
+    { MP_ROM_QSTR(MP_QSTR_TX),        MP_ROM_PTR(&pin_GPIO43) },
+    { MP_ROM_QSTR(MP_QSTR_RX),        MP_ROM_PTR(&pin_GPIO44) },
 
     // I2C header
     { MP_ROM_QSTR(MP_QSTR_I2C_SCL), MP_ROM_PTR(&pin_GPIO10) },
@@ -58,7 +58,7 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
 
     // Boot/User button
     { MP_ROM_QSTR(MP_QSTR_BOOT),     MP_ROM_PTR(&pin_GPIO0)  },
-    { MP_ROM_QSTR(MP_QSTR_BUTTON0),  MP_ROM_PTR(&pin_GPIO0)  }, // Optional Alias
+    { MP_ROM_QSTR(MP_QSTR_BUTTON0),  MP_ROM_PTR(&pin_GPIO0)  },
 
     // Primary bus pins
     { MP_ROM_QSTR(MP_QSTR_SCK),       MP_ROM_PTR(&pin_GPIO40) }, // Primary SPI (LCD)
@@ -73,37 +73,14 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_UART),    MP_ROM_PTR(&board_uart_obj) },
     { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display) },
 
-    // Fallback mapping for all GPIO pins
-    { MP_ROM_QSTR(MP_QSTR_IO0),   MP_ROM_PTR(&pin_GPIO0)  },
-    { MP_ROM_QSTR(MP_QSTR_IO1),   MP_ROM_PTR(&pin_GPIO1)  },
-    { MP_ROM_QSTR(MP_QSTR_IO2),   MP_ROM_PTR(&pin_GPIO2)  },
-    { MP_ROM_QSTR(MP_QSTR_IO3),   MP_ROM_PTR(&pin_GPIO3)  },
-    { MP_ROM_QSTR(MP_QSTR_IO4),   MP_ROM_PTR(&pin_GPIO4)  },
-    { MP_ROM_QSTR(MP_QSTR_IO5),   MP_ROM_PTR(&pin_GPIO5)  },
-    { MP_ROM_QSTR(MP_QSTR_IO6),   MP_ROM_PTR(&pin_GPIO6)  },
-    { MP_ROM_QSTR(MP_QSTR_IO7),   MP_ROM_PTR(&pin_GPIO7)  },
-    { MP_ROM_QSTR(MP_QSTR_IO8),   MP_ROM_PTR(&pin_GPIO8)  },
-    { MP_ROM_QSTR(MP_QSTR_IO9),   MP_ROM_PTR(&pin_GPIO9)  },
-    { MP_ROM_QSTR(MP_QSTR_IO10),  MP_ROM_PTR(&pin_GPIO10) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_IO11),  MP_ROM_PTR(&pin_GPIO11) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_IO12),  MP_ROM_PTR(&pin_GPIO12) },
-    { MP_ROM_QSTR(MP_QSTR_IO13),  MP_ROM_PTR(&pin_GPIO13) },
-    { MP_ROM_QSTR(MP_QSTR_IO14),  MP_ROM_PTR(&pin_GPIO14) },
-    { MP_ROM_QSTR(MP_QSTR_IO15),  MP_ROM_PTR(&pin_GPIO15) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_IO16),  MP_ROM_PTR(&pin_GPIO16) },
-    { MP_ROM_QSTR(MP_QSTR_IO17),  MP_ROM_PTR(&pin_GPIO17) },
-    { MP_ROM_QSTR(MP_QSTR_IO18),  MP_ROM_PTR(&pin_GPIO18) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_IO21),  MP_ROM_PTR(&pin_GPIO21) },
-    { MP_ROM_QSTR(MP_QSTR_IO38),  MP_ROM_PTR(&pin_GPIO38) },
-    { MP_ROM_QSTR(MP_QSTR_IO39),  MP_ROM_PTR(&pin_GPIO39) },
-    { MP_ROM_QSTR(MP_QSTR_IO40),  MP_ROM_PTR(&pin_GPIO40) },
-    { MP_ROM_QSTR(MP_QSTR_IO41),  MP_ROM_PTR(&pin_GPIO41) },
-    { MP_ROM_QSTR(MP_QSTR_IO42),  MP_ROM_PTR(&pin_GPIO42) },
-    { MP_ROM_QSTR(MP_QSTR_IO43),  MP_ROM_PTR(&pin_GPIO43) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_IO44),  MP_ROM_PTR(&pin_GPIO44) }, // User accessible
-    { MP_ROM_QSTR(MP_QSTR_IO45),  MP_ROM_PTR(&pin_GPIO45) },
-    { MP_ROM_QSTR(MP_QSTR_IO46),  MP_ROM_PTR(&pin_GPIO46) },
-    { MP_ROM_QSTR(MP_QSTR_IO47),  MP_ROM_PTR(&pin_GPIO47) },
-    { MP_ROM_QSTR(MP_QSTR_IO48),  MP_ROM_PTR(&pin_GPIO48) },
+    // User accessible
+    { MP_ROM_QSTR(MP_QSTR_IO10),  MP_ROM_PTR(&pin_GPIO10) },
+    { MP_ROM_QSTR(MP_QSTR_IO11),  MP_ROM_PTR(&pin_GPIO11) },
+    { MP_ROM_QSTR(MP_QSTR_IO15),  MP_ROM_PTR(&pin_GPIO15) },
+    { MP_ROM_QSTR(MP_QSTR_IO18),  MP_ROM_PTR(&pin_GPIO18) },
+    { MP_ROM_QSTR(MP_QSTR_IO19),  MP_ROM_PTR(&pin_GPIO19) },
+    { MP_ROM_QSTR(MP_QSTR_IO20),  MP_ROM_PTR(&pin_GPIO20) },
+    { MP_ROM_QSTR(MP_QSTR_IO43),  MP_ROM_PTR(&pin_GPIO43) },
+    { MP_ROM_QSTR(MP_QSTR_IO44),  MP_ROM_PTR(&pin_GPIO44) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
